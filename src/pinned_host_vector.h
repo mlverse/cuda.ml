ce67973ababdaeb30e3423991727c7e787187d66
Yitao Li <yitao@rstudio.com>
2021-07-12 23:14:40 +0000

    use modern C++ constructs from thrust
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ src/pinned_host_vector.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#if HAS_CUML
+
+#include <thrust/host_vector.h>
+#include <thrust/system/cuda/experimental/pinned_allocator.h>
+
+namespace cuml4r {
+
+template <typename T>
+using pinned_host_vector = thrust::host_vector<T, thrust::cuda::experimental::pinned_allocator<T>>;
+
+}  // namespace cuml4r
+
+#else
+
+#include "warn_cuml_missing.h"
+
+#endif
