8e8ba3f21063e452c2166ab38e206cc9622ae7d9
Yitao Li <yitao@rstudio.com>
2021-07-29 17:00:29 +0000

    PCA-related documentation improvements & bug fixes
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ man-roxygen/eigen-decomposition.R
@@ -0,0 +1,8 @@
+#' @param algo Eigen decomposition algorithm to be applied to the covariance
+#'   matrix. Valid choices are "dq" (divid-and-conquer method for symmetric
+#'   matrices) and "jacobi" (the Jacobi method for symmetric matrices).
+#'   Default: "dq".
+#' @param tol Tolerance for singular values computed by the Jacobi method.
+#'   Default: 1e-7.
+#' @param n_iters Maximum number of iterations for the Jacobi method.
+#'   Default: 15.
