e53468b20a49c4e0850cfd0b23dfb3f83745774e
Yitao Li <yitao@rstudio.com>
2021-08-17 19:26:38 +0000

    Rcpp implementation detail for FIL to work with XGBoostJSON and LightGBM formats (#74)
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -17,12 +17,12 @@
     .Call(`_cuml4r_fil_enabled`)
 }
 
-.treelite_load_xgboost_model <- function(filename, algo, output_class, threshold, storage_type, blocks_per_sm, threads_per_tree, n_items) {
-    .Call(`_cuml4r_treelite_load_xgboost_model`, filename, algo, output_class, threshold, storage_type, blocks_per_sm, threads_per_tree, n_items)
+.fil_load_model <- function(model_type, filename, algo, output_class, threshold, storage_type, blocks_per_sm, threads_per_tree, n_items) {
+    .Call(`_cuml4r_fil_load_model`, model_type, filename, algo, output_class, threshold, storage_type, blocks_per_sm, threads_per_tree, n_items)
 }
 
-.treelite_predict <- function(model, x, output_probabilities) {
-    .Call(`_cuml4r_treelite_predict`, model, x, output_probabilities)
+.fil_predict <- function(model, x, output_probabilities) {
+    .Call(`_cuml4r_fil_predict`, model, x, output_probabilities)
 }
 
 .kmeans <- function(x, k, max_iters, tol, init_method, centroids, seed, verbosity) {
--- src/fil.h
+++ src/fil.h
@@ -7,15 +7,14 @@
 
 namespace cuml4r {
 
-SEXP treelite_load_xgboost_model(
-  std::string const& filename, int const algo, bool const output_class,
-  float const threshold, int const storage_type, int const block_per_sm,
-  int const threads_per_tree, int const n_items);
+SEXP fil_load_model(int const model_type, std::string const& filename,
+                    int const algo, bool const output_class,
+                    float const threshold, int const storage_type,
+                    int const block_per_sm, int const threads_per_tree,
+                    int const n_items);
 
-Rcpp::NumericMatrix treelite_predict(
-    SEXP const& model,
-    Rcpp::NumericMatrix const& x,
-    bool const output_probabilities);
+Rcpp::NumericMatrix fil_predict(SEXP const& model, Rcpp::NumericMatrix const& x,
+                                bool const output_probabilities);
 
 }  // namespace cuml4r
 
