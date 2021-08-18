b9b9ed9e3390ed05663df8340a97b9ef3ba013fe
Yitao Li <yitao@rstudio.com>
2021-08-18 16:32:53 +0000

    R interface for FIL (#84)
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- R/fil.R
+++ R/fil.R
@@ -12,10 +12,170 @@
 #'   # run GPU-accelerated Forest Inference Library (FIL) functionalities
 #' } else {
 #'   message(
-#'     "FIL functionalities are disabled in the current installation of "
+#'     "FIL functionalities are disabled in the current installation of ",
 #'     "`cuml4r`."
 #'   )
 #' }
-#'
 #' @export
 cuml4r_fil_enabled <- .fil_enabled
+
+fil_match_model_type <- function(filename, model_type = c("xgboost", "lightgbm")) {
+  model_type <- match.arg(model_type)
+
+  switch(model_type,
+    xgboost = ifelse(grepl("\\.json$", filename), 1L, 0L),
+    lightgbm = 2L
+  )
+}
+
+fil_match_algo <- function(algo = c("auto", "naive", "tree_reorg", "batch_tree_reorg")) {
+  algo <- match.arg(algo)
+
+  switch(algo,
+    auto = 0L,
+    naive = 1L,
+    tree_reorg = 2L,
+    batch_tree_reorg = 3L
+  )
+}
+
+file_match_storage_type <- function(storage_type = c("auto", "dense", "sparse")) {
+  storage_type <- match.arg(storage_type)
+
+  switch(storage_type,
+    auto = 0L,
+    dense = 1L,
+    sparse = 2L
+  )
+}
+
+#' Load a XGBoost or LightGBM model file.
+#'
+#' Load a XGBoost or LightGBM model file using Treelite. The resulting model
+#' object can be used to perform high-throughput batch inference on new data
+#' points using the GPU acceleration functionality from the CuML Forest
+#' Inference Library (FIL).
+#'
+#' @template supervised-model-classification-or-regression-mode
+#' @param filename Path to the saved model file.
+#' @param model_type Format of the saved model file. Notice if \code{filename}
+#'   ends with ".json" and \code{model_type} is "xgboost", then 'cuml4r' will
+#'   assume the model file is in XGBoost JSON (instead of binary) format.
+#'   Default: "xgboost".
+#' @param algo Type of the algorithm for inference, must be one of the
+#'   following.
+#'     - "auto":
+#'         Choose the algorithm automatically. Currently 'batch_tree_reorg' is
+#'         used for dense storage, and 'naive' for sparse storage.
+#'     - "naive":
+#'         Simple inference using shared memory.
+#'     - "tree_reorg":
+#'         Similar to naive but with trees rearranged to be more coalescing-
+#'         friendly.
+#'     - "batch_tree_reorg":
+#'         Similar to 'tree_reorg' but predicting multiple rows per thread
+#'         block.
+#'   Default: "auto".
+#' @param threshold Class probability threshold for classification. Ignored for
+#'   regression tasks. Default: 0.5.
+#' @param storage_type In-memory storage format of the FIL model. Must be one of
+#'   the following.
+#'   - "auto":
+#'       Choose the storage type automatically,
+#'   - "dense":
+#'       Create a dense forest,
+#'   - "sparse":
+#'       Create a sparse forest. Requires \code{algo} to be 'naive' or 'auto'.
+#' @param threads_per_tree If >1, then have multiple (neighboring) threads infer
+#'   on the same tree within a block, which will improve memory bandwith near
+#'   tree root (but consuming more shared memory). Default: 1L.
+#' @param n_items Number of input samples each thread processes. If 0, then
+#'   choose (up to 4) that fit into shared memory. Default: 0L.
+#' @param blocks_per_sm Indicates how CuML should determine the number of thread
+#'   blocks to lauch for the inference kernel.
+#'   - 0:
+#'     Launches the number of blocks proportional to the number of data points.
+#'   - >= 1:
+#'     Attempts to lauch \code{blocks_per_sm} blocks for each streaming
+#'     multiprocessor.
+#'     This will fail if \code{blocks_per_sm} blocks result in more threads than
+#'     the maximum supported number of threads per GPU. Even if successful, it
+#'     is not guaranteed that \code{blocks_per_sm} blocks will run on an SM
+#'     concurrently.
+#'
+#' @examples
+#'
+#' library(cuml4r)
+#' library(xgboost)
+#'
+#' model_path <- file.path("", "tmp", "xgboost.model")
+#'
+#' model <- xgboost(
+#'   data = as.matrix(mtcars[which(names(mtcars) != "mpg")]),
+#'   label = as.matrix(mtcars["mpg"]),
+#'   max.depth = 6,
+#'   eta = 1,
+#'   nthread = 2,
+#'   nrounds = 20,
+#'   objective = "reg:squarederror"
+#' )
+#'
+#' xgb.save(model, model_path)
+#'
+#' model <- cuml_fil_load_model(
+#'   model_path,
+#'   mode = "regression",
+#'   model_type = "xgboost"
+#' )
+#'
+#' preds <- predict(model, mtcars[which(names(mtcars) != "mpg")])
+#'
+#' print(preds)
+#' @export
+cuml_fil_load_model <- function(filename,
+                                mode = c("classification", "regression"),
+                                model_type = c("xgboost", "lightgbm"),
+                                algo = c("auto", "naive", "tree_reorg", "batch_tree_reorg"),
+                                threshold = 0.5,
+                                storage_type = c("auto", "dense", "sparse"),
+                                threads_per_tree = 1L, n_items = 0L,
+                                blocks_per_sm = 0L) {
+  mode <- match.arg(mode)
+  model_type <- fil_match_model_type(filename, model_type)
+  algo <- fil_match_algo(algo)
+  storage_type <- file_match_storage_type(storage_type)
+
+  model <- .fil_load_model(
+    model_type = model_type,
+    filename = filename,
+    algo = algo,
+    output_class = identical(mode, "classification"),
+    threshold = as.numeric(threshold),
+    storage_type = storage_type,
+    threads_per_tree = as.integer(threads_per_tree),
+    n_items = as.integer(n_items),
+    blocks_per_sm = as.integer(blocks_per_sm)
+  )
+  class(model) <- c(class(model), "cuml_fil")
+
+  model
+}
+
+#' Predict using a model object created from \code{cuml_fil_load_model()}.
+#'
+#' Perform GPU-accelerated high-throughput batch inference on new data points
+#' using a XGBoost or LightGBM model created from \code{cuml_fil_load_model()}.
+#'
+#' @template model-with-numeric-input
+#' @param model The model object.
+#' @param output_probabilities Whether to output class probabilities. Only
+#'   applicable to binary classification tasks.
+#'
+#' @export
+predict.cuml_fil <- function(model, x, output_probabilities = FALSE, ...) {
+  .fil_predict(
+    model = model,
+    x = as.matrix(x),
+    output_probabilities = output_probabilities
+  )
+}
