22840a2e9d8f0141e4b7e88b9ead3329fd028996
Yitao Li <yitao@rstudio.com>
2021-08-27 20:16:35 +0000

    R interface for KNN classifier
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ man-roxygen/knn-algo-common.R
@@ -0,0 +1,3 @@
+#' @param nlist Number of cells to partition dataset into.
+#' @param nprobe At query time, the number of cells used for approximate nearest
+#'   neighbor search.
---
+++ man-roxygen/knn-algo-ivfsq.R
@@ -0,0 +1,4 @@
+#' @param qtype Quantizer type. Must be one of {"QT_8bit", "QT_4bit",
+#'   "QT_8bit_uniform", "QT_4bit_uniform", "QT_fp16", "QT_8bit_direct",
+#'   "QT_6bit"}.
+#' @param encode_residual Whether to encode residuals.
