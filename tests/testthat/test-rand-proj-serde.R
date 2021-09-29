d4d31fc5d7100ecec3abba79751e15d240824c47
Yitao Li <yitao@rstudio.com>
2021-09-29 16:24:58 +0000

    R interface for (de)serialization of random projection models (#135)
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ tests/testthat/test-rand-proj-serde.R
@@ -0,0 +1,26 @@
+test_that("random projection model can be serialized and unserialized correctly", {
+  has_mlbench <- require("mlbench")
+  stopifnot(has_mlbench)
+
+  data(Vehicle)
+  data <- Vehicle[, which(names(Vehicle) != "Class")]
+
+  model <- cuda_ml_rand_proj(data, n_components = 4)
+  model_state <- cuda_ml_serialize(model)
+
+  actual_transformed_data <- callr::r(
+    function(model_state, data) {
+     library(cuda.ml)
+
+      model <- cuda_ml_unserialize(model_state)
+
+      cuda_ml_transform(model, data)
+    },
+    args = list(
+      model_state = model_state,
+      data = data
+    )
+  )
+
+  expect_equal(actual_transformed_data, model$transformed_data)
+})
