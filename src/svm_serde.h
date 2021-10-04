518b1cbb671bf26bd38958c37bf868a54a6a1f59
Yitao Li <yitao@rstudio.com>
2021-10-04 16:47:34 +0000

    replace svm_serde template specializations with function overloads for simplicity
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- src/svm_serde.h
+++ src/svm_serde.h
@@ -30,36 +30,18 @@ constexpr auto kSvmModelSupportIdxes = "support_idx";
 constexpr auto kSvmModelNumClasses = "n_classes";
 constexpr auto kSvmModelUniqueLabels = "unique_labels";
 
-template <typename T>
-Rcpp::List getState(T const&) = delete;
-
-template <typename T>
-Rcpp::List getState(T const&, raft::handle_t const&) = delete;
-
-template <>
 Rcpp::List getState(MLCommon::Matrix::KernelParams const& kernel_params);
 
-template <>
 Rcpp::List getState(ML::SVM::svmParameter const& svm_params);
 
-template <>
 Rcpp::List getState(ML::SVM::svmModel<double> const& svm_model,
                     raft::handle_t const& handle);
 
-template <typename T>
-void setState(T&, Rcpp::List const&) = delete;
-
-template <typename T>
-void setState(T&, raft::handle_t const&, Rcpp::List const&) = delete;
-
-template <>
 void setState(MLCommon::Matrix::KernelParams& kernel_params,
               Rcpp::List const& state);
 
-template <>
 void setState(ML::SVM::svmParameter& svm_params, Rcpp::List const& state);
 
-template <>
 void setState(ML::SVM::svmModel<double>& svm_model,
               raft::handle_t const& handle, Rcpp::List const& state);
 
