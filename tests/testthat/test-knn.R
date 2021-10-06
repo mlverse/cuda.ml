edc092048485646c7d4bb64092691d76a67cf3cf
Yitao Li <yitao@rstudio.com>
2021-10-06 21:41:44 +0000

    functional test case for KNN regressor
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tests/testthat/test-knn.R
+++ tests/testthat/test-knn.R
@@ -13,10 +13,10 @@ blobs_df <- blobs %>%
   )
 
 test_blob_sz <- 10
-test_blobs_df <- gen_blobs(test_blob_sz, centers) %>%
-  as.data.frame()
 
 test_that("KNN classifier works as expected", {
+  test_blobs_df <- gen_blobs(test_blob_sz, centers) %>%
+    as.data.frame()
   for (algo in c("brute", "ivfflat", "ivfpq", "ivfsq")) {
     model <- cuda_ml_knn(
       label ~ ., blobs_df, algo = algo, metric = "euclidean"
@@ -31,4 +31,33 @@ test_that("KNN classifier works as expected", {
       label = algo
     )
   }
+})
+
+test_that("KNN regressor works as expected", {
+  resps <- seq_along(centers) %>%
+    sapply(function(x) rep(exp(-x), blob_sz)) %>%
+    c()
+  train_df <- blobs %>%
+    as.data.frame() %>%
+    cbind(y = resps)
+  test_blobs <- gen_blobs(test_blob_sz, centers)
+
+  cuda_ml_knn_regressor <- cuda_ml_knn(
+    y ~ ., data = train_df, algo = "brute", metric = "euclidean", neighbors = 5L
+  )
+  cuda_ml_knn_regressor_preds <- predict(
+    cuda_ml_knn_regressor, as.data.frame(test_blobs)
+  )
+
+  sklearn_knn_regressor <- sklearn$neighbors$KNeighborsRegressor(
+    n_neighbors = 5L, algorithm = "brute", metric = "euclidean"
+  )
+  sklearn_knn_regressor$fit(X = blobs, y = resps)
+  sklearn_knn_regressor_preds <- sklearn_knn_regressor$predict(
+    as.matrix(test_blobs)
+  )
+
+  expect_equal(
+    cuda_ml_knn_regressor_preds$.pred, as.numeric(sklearn_knn_regressor_preds)
+  )
 })
