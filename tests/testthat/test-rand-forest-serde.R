5acd3c93e1d341e1457247a96b7ce5134953f3b0
Yitao Li <yitao@rstudio.com>
2021-10-08 16:43:15 +0000

    additional compatibility changes for rand_forest when working with RAPIDS cuML v21.06 or below
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tests/testthat/test-rand-forest-serde.R
+++ tests/testthat/test-rand-forest-serde.R
@@ -7,26 +7,31 @@ test_that("random forest classifier can be serialized and unserialized correctly
   data <- iris[-which(names(iris) == "Species")]
 
   expected_preds <- predict(model, data)
-  expected_cls_probs <- predict(
-    model, data,
-    output_class_probabilities = TRUE
-  )
   actual_preds <- predict_in_sub_proc(
     model_state,
     data = data,
     expected_mode = "classification",
     expected_model_cls = "cuda_ml_rand_forest"
   )
-  actual_cls_probs <- predict_in_sub_proc(
-    model_state,
-    data = data,
-    expected_mode = "classification",
-    expected_model_cls = "cuda_ml_rand_forest",
-    additional_predict_args = list(output_class_probabilities = TRUE)
-  )
 
   expect_equal(expected_preds, actual_preds)
-  expect_equal(expected_cls_probs, actual_cls_probs, tolerance = 1e-4, scale = 1)
+
+  if (as.integer(cuML_minor_version()) >= 8) {
+    # class probabilities output was not supported in earlier versions of RAPIDS
+    # cuML
+    expected_cls_probs <- predict(
+      model, data,
+      output_class_probabilities = TRUE
+    )
+    actual_cls_probs <- predict_in_sub_proc(
+      model_state,
+      data = data,
+      expected_mode = "classification",
+      expected_model_cls = "cuda_ml_rand_forest",
+      additional_predict_args = list(output_class_probabilities = TRUE)
+    )
+    expect_equal(expected_cls_probs, actual_cls_probs, tolerance = 1e-3, scale = 1)
+  }
 })
 
 test_that("random forest regressor can be serialized and unserialized correctly", {
