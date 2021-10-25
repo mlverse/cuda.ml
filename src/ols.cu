a3faca58403e6ede5e76b4f103d0837f93d05a21
Yitao Li <yitao@rstudio.com>
2021-10-25 21:17:11 +0000

    fix pointer type for OLS intercept term
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- src/ols.cu
+++ src/ols.cu
@@ -11,7 +11,9 @@ __host__ Rcpp::List ols_fit(Rcpp::NumericMatrix const& x,
                             bool const normalize_input, int const algo) {
   using namespace std::placeholders;
 
-  return lm_fit(x, y, /*intercept_type=*/lm::InterceptType::DEVICE,
+  return lm_fit(x, y, /*intercept_type=*/
+                        fit_intercept ? lm::InterceptType::DEVICE
+                                      : lm::InterceptType::HOST,
                 fit_intercept, normalize_input,
                 /*fit_impl=*/std::bind(detail::ols_fit_impl, _1, _2, algo));
 }
