512fd563f7632abee9d18e16f70106277fc37c71
Yitao Li <yitao@rstudio.com>
2021-10-26 21:36:07 +0000

    R interface for LASSO regression
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- R/lm.R
+++ R/lm.R
@@ -1,3 +1,22 @@
+validate_lm_input <- function(processed) {
+  hardhat::validate_outcomes_are_univariate(processed$outcomes)
+  hardhat::validate_outcomes_are_numeric(processed$outcomes)
+  hardhat::validate_predictors_are_numeric(processed$predictors)
+
+  predictors <- as.matrix(processed$predictors)
+  if (ncol(predictors) < 1) stop("Predictors must contain at least 1 feature.")
+  if (nrow(predictors) < 2) stop("At least 2 samples are required.")
+}
+
+new_linear_model <- function(cls, xptr, ...) {
+  new_model(
+    cls = c(cls, "cuda_ml_linear_model"),
+    mode = "regression",
+    xptr = xptr,
+    ...
+  )
+}
+
 #' Make predictions on new data points.
 #'
 #' Make predictions on new data points using a linear model.
---
+++ tests/testthat/test-lasso.R
@@ -0,0 +1,66 @@
+context("Least Absolute Shrinkage and Selection Operator")
+
+test_that("LASSO regressor works as expected", {
+  for (normalize_input in c(FALSE, TRUE)) {
+    for (fit_intercept in c(FALSE, TRUE)) {
+      if (!fit_intercept && normalize_input) {
+        next
+      }
+
+      if (!fit_intercept) {
+        input <- mtcars
+        input[, which(names(mtcars) != "mpg")] <- scale(
+          input[, which(names(mtcars) != "mpg")], scale = FALSE
+        )
+        input <- as.matrix(input)
+      } else {
+        input <- as.matrix(mtcars)
+      }
+
+      if (normalize_input) {
+        sklearn_scaler <- sklearn$preprocessing$StandardScaler(
+          copy = TRUE, with_mean = TRUE, with_std = TRUE
+        )
+        sklearn_scaler$fit(as.matrix(mtcars[, which(names(mtcars) != "mpg")]))
+        sklearn_predictors <- sklearn_scaler$transform(
+          as.matrix(mtcars[, which(names(mtcars) != "mpg")])
+        )
+      } else {
+        sklearn_predictors <- as.matrix(input[, which(names(mtcars) != "mpg")])
+      }
+
+      sklearn_lasso_regressor <- sklearn$linear_model$Lasso(
+        alpha = 1e-3,
+        max_iter = 10000,
+        tol = 1e-4,
+        fit_intercept = fit_intercept
+      )
+      sklearn_lasso_regressor$fit(
+        X = sklearn_predictors,
+        y = mtcars$mpg
+      )
+      sklearn_lasso_regressor_preds <- sklearn_lasso_regressor$predict(
+        sklearn_predictors
+      )
+
+      cuda_ml_lasso_regressor <- cuda_ml_lasso(
+        mpg ~ ., input,
+        alpha = 1e-3,
+        max_iter = 10000,
+        tol = 1e-4,
+        fit_intercept = fit_intercept,
+        normalize_input = normalize_input
+      )
+      cuda_ml_lasso_regressor_preds <- predict(
+        cuda_ml_lasso_regressor, input
+      )
+
+      expect_equal(
+        cuda_ml_lasso_regressor_preds$.pred,
+        as.numeric(sklearn_lasso_regressor_preds),
+        tol = 0.05,
+        scale = 1
+      )
+    }
+  }
+})
