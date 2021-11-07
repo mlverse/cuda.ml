a9f5027fa9745385a778f6c8d3555371e570067f
Yitao Li <yitao@rstudio.com>
2021-11-07 23:58:43 +0000

    address post-submission feedback from CRAN
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- src/warn_cuml_missing.h
+++ src/warn_cuml_missing.h
@@ -1,2 +1,9 @@
-#pragma message( \
-  "`cuml4r` requires a valid RAPIDS installation. Please follow https://rapids.ai/start.html to install RAPIDS first. `cuml4r` must be installed and run from an environment containing a valid CUML_PREFIX env variable (e.g., '/home/user/anaconda3/envs/rapids-21.06' or similar).")
+// NOTE: disabling this message for now per human feedback from CRAN
+
+// #pragma message(                                                         \
+//   "`cuml4r` requires a valid RAPIDS installation. "                      \
+//   "Please follow https://rapids.ai/start.html to install RAPIDS first. " \
+//   "`cuml4r` must be installed and run from an environment containing "   \
+//   "a valid CUML_PREFIX env variable (e.g., "                             \
+//   "'/home/user/anaconda3/envs/rapids-21.06' or similar)."                \
+// )
