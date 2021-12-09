e12e83e0781bd1d5cbb9bcc70274ceb12449873f
Yitao Li <yitao@rstudio.com>
2021-12-09 20:07:00 +0000

    add libcuml 21.12 to GitHub CI workflow
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tools/config/configure.R
+++ tools/config/configure.R
@@ -64,6 +64,9 @@ libcuml_versions <- list(
   ),
   "21.10" = list(
     "11" = "https://github.com/mlverse/libcuml-builds/releases/download/v21.10-cuda11.2.1/libcuml-21.10-cuda11.2.1.zip"
+  ),
+  "21.12" = list(
+    "11" = "https://github.com/mlverse/libcuml-builds/releases/download/v21.12-cuda11.2.1/libcuml-21.12-cuda11.2.1.zip"
   )
 )
 
