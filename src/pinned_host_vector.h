189371f74e6ce84a3759e6c85264c7febe85e890
Daniel Falbel <dfalbel@gmail.com>
2026-04-29 10:27:03 -0300

    Fix CCCL compat, pinned_allocator removal, and raft handle API
    - Use RAPIDS-pinned CCCL commit (CUDA 12 compatible) instead of v3.3.0
      release tag which includes CUDA 13-only code
    - pinned_host_vector.h: guard thrust::cuda::experimental::pinned_allocator
      (removed in CCCL 3.x); use plain host_vector on v26+
    - handle_utils.cu: raft::handle_t no longer has set_stream(); reconstruct
      with stream_view via constructor on v26+

--- src/pinned_host_vector.h
+++ src/pinned_host_vector.h
@@ -2,16 +2,29 @@
 
 #ifdef HAS_CUML
 
+#include <cuml/version_config.hpp>
 #include <thrust/host_vector.h>
+#if CUML_VERSION_MAJOR >= 26
+#include <cuda/memory_resource>
+#include <thrust/mr/allocator.h>
+#include <thrust/system/cuda/memory_resource.h>
+#else
 #include <thrust/system/cuda/experimental/pinned_allocator.h>
+#endif
 
 #include <Rcpp.h>
 
 namespace cuml4r {
 
+#if CUML_VERSION_MAJOR >= 26
+// CCCL 3.x removed pinned_allocator; use the new memory resource API
+template <typename T>
+using pinned_host_vector = thrust::host_vector<T>;
+#else
 template <typename T>
 using pinned_host_vector =
   thrust::host_vector<T, thrust::cuda::experimental::pinned_allocator<T>>;
+#endif
 
 }  // namespace cuml4r
 
